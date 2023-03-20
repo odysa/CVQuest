68f364364d07a672fbb7879538f5f7ea5de88ac5
odysa <piensengxv@gmail.com>
2023-03-20 08:31:18 +0000

    draft badge

--- server.py
+++ server.py
@@ -3,12 +3,14 @@
 import gradio as gr
 from application.interview import InterviewQuestionMaker
 
+
 def generate_interview_questions(resume_pdf):
     """Generate interview questions based on the given resume PDF."""
     question_maker = InterviewQuestionMaker()
     questions = question_maker.create_questions(resume_pdf.name)
     questions_html = display_questions(questions)
     return questions_html
+
 
 def display_questions(questions):
     """Display the questions in an HTML format."""
@@ -22,6 +24,7 @@ def display_questions(questions):
     html += "</div>"
     return html
 
+
 STYLE = """
 .block {
     border-bottom: 0.5px solid lightgrey;
@@ -33,15 +36,23 @@ STYLE = """
 }
 """
 
+INTRO_MD = """
+# CVQuest -- Generate personal interview questions from your resume
+
+[![GitHub stars](https://img.shields.io/github/stars/Naereen/StrapDown.js.svg?style=social&label=Star&maxAge=2592000)](https://github.com/odysa/CVQuest)![GitHub followers](https://img.shields.io/github/followers/Naereen.svg?style=social&label=Follow&maxAge=2592000)
+
+[CVQuest](https://github.com/odysa/CVQuest) is an open-source project. If you want to control you know data, please run locally or deploy your own. We will **NOT** store your personal data.
+
+Sponsor: Want preparations beyond interview questions? Check out __[Baynana's AI resume builder](https://baynana.co/)__ to 10x your interview rate.
+"""
+
 with gr.Blocks(css=STYLE) as demo:
-    gr.Markdown("# Generate personal interview questions with your resume")
-    gr.Markdown("Want preparations beyond interview questions? "
-                "Check out __[Baynana's AI resume builder](https://baynana.co/)__ "
-                "to 10x your interview rate. "
-                "_We will not store your resume data._")
+    gr.Markdown(INTRO_MD)
+
     input_pdf = gr.inputs.File(type="file")
     generate_btn = gr.Button("Generate")
     output_html = gr.outputs.HTML()
-    generate_btn.click(fn=generate_interview_questions, inputs=input_pdf, outputs=output_html)
+    generate_btn.click(fn=generate_interview_questions,
+                       inputs=input_pdf, outputs=output_html)
 
 demo.launch()
