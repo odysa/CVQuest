ed583606eb3496da438ba52275bac89661913d7f
odysa <piensengxv@gmail.com>
2023-03-21 20:29:10 -0700

    feat: use svelte as web ui (#15)
    * add ga
    
    * add ga
    
    * add ga
    
    * init svelte
    
    * basic layout
    
    * draft page
    
    * finish frontend
    
    * remove pnpm lock
    
    * fix pylint

--- main.py
+++ main.py
@@ -1,64 +1,40 @@
 """
-This script provides a command-line interface to process PDF resumes and generate
-interview questions or convert the resumes to JSON format using the `InterviewQuestionMaker`
-and `ResumeJsonParser` classes.
-
-The script offers two commands:
-- `json`: Converts a given PDF resume file to JSON format.
-- `q`: Generates interview questions based on the content of a given PDF resume file.
-
-Example usage:
-
-python script_name.py json /path/to/resume.pdf
-python script_name.py q /path/to/resume.pdf
-
+This is the server of CVQuest
 """
+from fastapi import FastAPI, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 
-import typer
-from rich import print as pprint
-from rich.progress import Progress, SpinnerColumn, TextColumn
 from application.interview import InterviewQuestionMaker
-from application.parser import ResumeJsonParser
+from application.utils import OpenAIConfig
 
-app = typer.Typer()
+app = FastAPI()
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-json_parser: ResumeJsonParser = ResumeJsonParser()
-question_maker: InterviewQuestionMaker = InterviewQuestionMaker()
+static = FastAPI()
+static.mount('', StaticFiles(directory="./site/build",
+                             html=True), name="static")
+
+question_maker = InterviewQuestionMaker(config=OpenAIConfig(temperature=0.7))
 
 
-@app.command()
-def json(file_path: str):
+@app.post("/questions/")
+async def create_questions(file: UploadFile):
     """
-    Convert a PDF resume file to JSON format.
+    Create interview questions from a text file uploaded via HTTP POST.
 
     Args:
-        file_path (str): The path to the PDF resume file.
+        file (UploadFile): The uploaded text file.
+
+    Returns:
+        str: The generated interview questions.
     """
-    with Progress(
-        SpinnerColumn(),
-        TextColumn("[progress.description]{task.description}"),
-        transient=True,
-    ) as progress:
-        progress.add_task(description="Processing...", total=None)
-        pprint(json_parser.pdf2json(file_path))
-
-
-@app.command("q")
-def question(file_path: str):
-    """
-    Generate interview questions based on the content of a PDF resume file.
-
-    Args:
-        file_path (str): The path to the PDF resume file.
-    """
-    with Progress(
-        SpinnerColumn(),
-        TextColumn("[progress.description]{task.description}"),
-        transient=True,
-    ) as progress:
-        progress.add_task(description="Processing...", total=None)
-        pprint(question_maker.create_questions(file_path))
-
-
-if __name__ == "__main__":
-    app()
+    answers = question_maker.create_questions(file.filename)
+    return answers
