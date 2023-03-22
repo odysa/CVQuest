5579cc93a96ddbf3396cb2eaf6c3704beafa3274
Huan Xu <hxu296@wisc.edu>
2023-03-22 17:23:35 -0500

    fix: run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-uvicorn main:app --port 8989 & 
-uvicorn main:static --host 0.0.0.0 --port 8080 &
-sleep infinity
-=======
 uvicorn main:little_nginx --host "0.0.0.0" --port 8080
->>>>>>> 903a89335b7e9bd2cdffaf6079ba881c13ffa5b9
